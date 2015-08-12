1574e9baf2553c3b39cf19e3f92b426f899de50d
David Johnston <dajohnston@ucdavis.edu>
2015-08-11 22:38:39 -0700

    complete number rep in binary

--- fundamentals/bit_manipulation/nbits_in_int.py
+++ fundamentals/bit_manipulation/nbits_in_int.py
@@ -17,7 +17,13 @@ class NBitsInInt(object):
 class BinaryRepresentation(object):
 
     @staticmethod
-    def binary_rep(n):
+    def integer_rep(n):
+        """
+        :param n: a non-negative integer
+        :return: string representation of it's binary value,
+           e.g., 2 --> "10"
+                 255 --> "11111111"
+        """
         if n < 0:
             raise ValueError("let's not think about negative numbers yet")
 
@@ -35,15 +41,57 @@ class BinaryRepresentation(object):
 
         return "".join(results)
 
+    @staticmethod
+    def decimal_rep(dec):
+        """
+        :param dec: a decimal value between 0 and 1
+        :return: a string representation of it's binary value
+           e.g., 0.5 --> ".1"
+                 0.25 -> ".01"
+                 0.75 -> ".11"
+        """
+        if dec <= 0 or dec >= 1:
+            raise ValueError("input must be a decimal between 0 and 1")
 
+        results = ["."]
+
+        while dec > 0 and len(results) <= 32:
+            dec *= 2
+            if dec >= 1.0:
+                results.append("1")
+                dec -= 1.0
+            else:
+                results.append("0")
+
+        return "".join(results)
+
+    @staticmethod
+    def number_rep(num):
+        """
+        :param num: a non-negative real number
+        :return: a string representation of it's binary value
+           e.g., 11.25 --> "1011.01"
+        """
+
+        wholenum = int(num)
+        dec = num - float(wholenum)
+
+        return "{0}{1}".format(BinaryRepresentation.integer_rep(wholenum), BinaryRepresentation.decimal_rep(dec))
 
 
 def main():
     print NBitsInInt.nbits(7)
     print NBitsInInt.nbits(255)
+    print "-" * 20
+    print BinaryRepresentation.integer_rep(7)
+    print BinaryRepresentation.integer_rep(11)
+    print "-" * 20
+    print BinaryRepresentation.decimal_rep(0.5)
+    print BinaryRepresentation.decimal_rep(0.25)
+    print BinaryRepresentation.decimal_rep(0.75)
+    print "-" * 20
+    print BinaryRepresentation.number_rep(11.25)
 
-    print BinaryRepresentation.binary_rep(7)
-    print BinaryRepresentation.binary_rep(11)
 
 
 if __name__ == "__main__":
--- tests/bit_manipulation/test_nbits_in_int.py
+++ tests/bit_manipulation/test_nbits_in_int.py
@@ -11,6 +11,14 @@ class TestBinary(TestCase):
         self.assertEquals(8, NBitsInInt.nbits(255))
 
     def test_binary_rep(self):
-        self.assertEquals("1011", BinaryRepresentation.binary_rep(11))
-        self.assertEquals("", BinaryRepresentation.binary_rep(0))
-        self.assertEquals("11111111", BinaryRepresentation.binary_rep(255))
+        self.assertEquals("1011", BinaryRepresentation.integer_rep(11))
+        self.assertEquals("", BinaryRepresentation.integer_rep(0))
+        self.assertEquals("11111111", BinaryRepresentation.integer_rep(255))
+
+    def test_decimal_rep(self):
+        self.assertEquals(".01", BinaryRepresentation.decimal_rep(0.25))
+        self.assertEquals(".1", BinaryRepresentation.decimal_rep(0.5))
+        self.assertEquals(".001", BinaryRepresentation.decimal_rep(0.125))
+
+    def test_number_rep(self):
+        self.assertEquals("1011.001", BinaryRepresentation.number_rep(11.125))
